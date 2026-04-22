57b7b0d8ca7937d0d58f39e9c8be34160939ddd1
fauziramdani <fauziramdani234@gmail.com>
2026-04-22 14:43:00 +0700

    rollback

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
 
 import react from '@astrojs/react';
 
@@ -8,8 +7,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: netlify(),
   integrations: [react()],
 
   vite: {
